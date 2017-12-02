1bf739ea7f52f4734432637b661bd27953a8d995
rounakdatta12@gmail.com <rounakdatta12@gmail.com>
2017-12-02 11:32:03 +0530

    d2

--- d2/d2.py
+++ d2/d2.py
@@ -1,11 +1,10 @@
 import fileinput
 
 s = 0
+
 for line in fileinput.input():
-	#print(int(max(line.split(), key=len)) - int(min(line.split(), key=len)))
 	line = ((line.split()))
 	line = [int(i) for i in line]
-	#print(max(line) - min(line))
 	s += (max(line) - min(line))
 
 print(s)
