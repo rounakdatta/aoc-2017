7ac9d2dc1c30b86d7fa016c1b6ff70b7a12d8bfd
Rounak Datta <rounakdatta12@gmail.com>
2017-12-04 21:10:52 +0530

    -r unnecessary sts

--- d4/d4p2.py
+++ d4/d4p2.py
@@ -6,7 +6,6 @@ count = 0
 for line in fileinput.input():
 	line = ((line.split()))
 
-	eqv = 0
 	for i in range(0, len(line)):
 		line[i] = (list(line[i]))
 		line[i].sort()
@@ -17,12 +16,10 @@ for line in fileinput.input():
 			if(line[i] == line[j]):
 				same = 1
 				break
-			if(same == 1):
-				break
+		if(same == 1):
+			break
 	if(same == 0):
 		count += 1
 	same = 0
-	#line = [int(i) for i in line]
-	#s += (max(line) - min(line))
 
 print(count)
