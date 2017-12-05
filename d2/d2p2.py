4fa9d1178a87d79fc0accf2fa8bb11bbf0b1c8e2
rounakdatta12@gmail.com <rounakdatta12@gmail.com>
2017-12-05 17:49:00 +0530

    d2 p2

--- d2/d2p2.py
+++ d2/d2p2.py
@@ -3,9 +3,18 @@ import fileinput
 s = 0
 
 for line in fileinput.input():
+	b = 0
 	line = ((line.split()))
 	line = [int(i) for i in line]
-	s += (max(line) - min(line))
+	for i in range(0, len(line)):
+		for j in range(0, len(line)):
+			if(((line[i] % line [j]) == 0) and i != j):
+				b = 1
+				print(line[i], " and ", line[j])
+				s += (line[i] // line[j])
+				break
+			if(b ==1):
+				break
 
 print(s)
 	
