82d7603f03155a40f49ac53e0f1e047a0042aea8
rounakdatta12@gmail.com <rounakdatta12@gmail.com>
2017-12-17 22:25:42 +0530

    d15

--- d16/d16p1.py
+++ d16/d16p1.py
@@ -1,24 +1,44 @@
-programs = ['a', 'b', 'c', 'd', 'e']
-#for i in range(97, 113):
-#  programs.append(chr(i))
+programs = []
+#programs = ['a', 'b', 'c', 'd', 'e']
+for i in range(97, 113):
+	  programs.append(chr(i))
 
-x = input()
-x= x.split(',')
 
-for query in x:
-  if(query[0] == 's'):
-    temp = programs[len(programs) - int(query[1:]):]
-    programs = programs[:len(programs) - int(query[1:])]
-    programs = temp + programs
-    print(''.join(programs))
-  if(query[0] == 'x'):
-    params = query.split('/')
-    programs[int(params[0][1:])], programs[int(params[1])] = programs[int(params[1])], programs[int(params[0][1:])]
-    print(''.join(programs))
-    #print(params)
-  if(query[0] == 'p'):
-    params = query.split('/')
-    programs[programs.index(params[0][1:])], programs[programs.index(params[1])] = programs[programs.index(params[1])], programs[programs.index(params[0][1:])]
-    print(''.join(programs))
+x = []
+txt = ''
+with open("input.txt") as f:
+	while True:
+		c = f.read(1)
+		if not c:
+			x.append(txt)
+			break
+		if(c == ','):
+			x.append(txt)
+			txt = ''
+		else:
+			txt = txt + c
 
-#print(''.join(programs))
+for i in range(len(x)):
+	query = x[i]
+
+	if(query[0] == 's'):
+		temp = programs[len(programs) - int(query[1:]):]
+		programs = programs[:len(programs) - int(query[1:])]
+		programs = temp + programs
+		#print(''.join(programs))
+
+	elif(query[0] == 'x'):
+		params = query.split('/')
+		programs[int(params[0][1:])], programs[int(params[1])] = programs[int(params[1])], programs[int(params[0][1:])]
+		#print(''.join(programs))
+
+	elif(query[0] == 'p'):
+		params = query.split('/')
+		el1 = ''.join(params[0][1:])
+		el2 = params[1]
+		el1 = programs.index(el1)
+		el2 = programs.index(el2)
+		programs[el1], programs[el2] = programs[el2], programs[el1]
+		#print(''.join(programs))
+
+print(''.join(programs))
